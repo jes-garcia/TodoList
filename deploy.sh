40b229fe38dfd8f8d06236f30e30f58161a49a6e
jes-garcia <x22154060@student.ncirl.ie>
2023-04-17 01:41:18 +0100

    CRLF to LF

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,15 @@
-#!/usr/bin/env bash
-CURRENT_INSTANCE="$(sudo docker ps -a -q --filter ancestor="$IMAGE_NAME" --format="{{.ID}}")"
+CURRENT_INSTANCE=$(sudo docker ps -a -q --filter ancestor="$IMAGE_NAME" --format="{{.ID}}")
 if [ "$CURRENT_INSTANCE" ]
 then
-    sudo docker rm "$(sudo docker stop "$CURRENT_INSTANCE")"
+    sudo docker rm $(sudo docker stop $CURRENT_INSTANCE)
 fi
-sudo docker pull "$IMAGE_NAME"
-CONTAINER_EXIST=$(sudo docker ps -a | grep "$CONTAINER_NAME")
+
+sudo docker pull $IMAGE_NAME
+
+CONTAINER_EXIST=$(sudo docker ps -a | grep $CONTAINER_NAME)
 if [ "$CONTAINER_EXIST" ]
 then
-    sudo docker rm "$CONTAINER_NAME"
+    sudo docker rm $CONTAINER_NAME
 fi
-sudo docker run -p 3000:3000 -d --name "$CONTAINER_NAME $IMAGE_NAME"
+
+sudo docker run -p 3000:3000 -d --name $CONTAINER_NAME $IMAGE_NAME
